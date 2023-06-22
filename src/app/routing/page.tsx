5fa322416c211b1f7885d3311d72af0a3ad1750e
ria-ahyoung <ria.ahyoung@gmail.com>
2023-06-22 21:19:57 +0900

    feat: operate static routing in Next.js

---
+++ src/app/routing/page.tsx
@@ -0,0 +1,8 @@
+export default async function Routing() {
+  return (
+    <main>
+      <h2>Let's learn about how Page Routing in Next 13 version.</h2>
+      <p>👷‍♀️ File-System based Router</p>
+    </main>
+  );
+}
---
+++ src/app/routing/static/page.tsx
@@ -0,0 +1,11 @@
+export default async function StaticRouting() {
+  return (
+    <main>
+      <h2>This page is operates with SSG !</h2>
+      <p>
+        👷‍♀️ <strong>Static Route</strong> in Next.js with{" "}
+        <u>file-system based router</u> .
+      </p>
+    </main>
+  );
+}
