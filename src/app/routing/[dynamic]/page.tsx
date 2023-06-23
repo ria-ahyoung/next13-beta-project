a29cb52e1f1deba70669f3316f076594d94ee510
ria-ahyoung <ria.ahyoung@gmail.com>
2023-06-23 23:00:18 +0900

    fix: update with Semantic Tag

--- src/app/routing/[dynamic]/page.tsx
+++ src/app/routing/[dynamic]/page.tsx
@@ -3,8 +3,16 @@ import { DynamicRoutingProps } from "./type";
 export default async function DynamicRouting({ params }: DynamicRoutingProps) {
   return (
     <main>
-      <h2>This page is operates with Dynamic Route!</h2>
+      <h2>
+        This page is operates with <mark>Dynamic Route!</mark>
+      </h2>
       <p>👷‍♀️ Dynamiclly Passed props : {params.dynamic}</p>
+      <span>
+        ⚙️ params 값이 동적으로 결정
+        <br />
+        <br />
+        <dd> → pre-rendering 된 HTMl 골격과 SSR 조합으로 동작합니다.</dd>
+      </span>
     </main>
   );
 }
