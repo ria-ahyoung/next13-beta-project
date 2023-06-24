1f7a96007d5086b564c97a8e9652b81f75382adb
ria-ahyoung <ria.ahyoung@gmail.com>
2023-06-24 23:52:02 +0900

    feat: dynamic routing with static params

--- src/app/routing/[dynamic]/page.tsx
+++ src/app/routing/[dynamic]/page.tsx
@@ -13,6 +13,22 @@ export default async function DynamicRouting({ params }: DynamicRoutingProps) {
         <br />
         <dd> → pre-rendering 된 HTMl 골격과 SSR 조합으로 동작합니다.</dd>
       </span>
+
+      <br />
+
+      <span>
+        💡 다이나믹 경로를 사용하는 페이지 중 원하는 특정 경로의 페이지를 미리
+        정적으로 생성할 수 있습니다.
+      </span>
     </main>
   );
+}
+
+// dynamic_with_static_params
+export function generateStaticParams() {
+  const params = ["you", "can", "generate", "static", "parmas"];
+  return params.map((param) => ({
+    // 컴포넌트에 전달되는 params 형태로 동일하게 작성
+    dynamic: param,
+  }));
 }
