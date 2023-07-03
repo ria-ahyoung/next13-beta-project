f202b3e8b60a608717ca45f1f74d769c66b1c09d
ria-ahyoung <ria.ahyoung@gmail.com>
2023-07-03 14:24:23 +0900

    feat: `nested layout` within sub directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
+
 type RootLayout = {
   children: React.ReactNode;
 };
 
+/** 하위 폴더 경로에서도 별도의 레이아웃 페이지를 생성할 수 있습니다. */
 export default function RootLayout({ children }: RootLayout) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <h1>Next 13 Practice Project</h1>
+          <nav>
+            <Link href="/routing">Routing</Link>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
