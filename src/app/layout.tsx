b41afedadba6f97a6c22410634582297f9569601
ria-ahyoung <ria.ahyoung@gmail.com>
2023-07-19 20:29:56 +0900

    feat: setup built-in `google font`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { Inter } from "next/font/google";
+
 import "./globals.css";
 import style from "./layout.module.css";
 
@@ -24,11 +26,14 @@ type RootLayout = {
   children: React.ReactNode;
 };
 
+/** next/font는 모든 구글 글꼴 파일이 내장되어 있어 레이아웃 쉬프트 없이 폰트를 사용할 수 있습니다. */
+const inter = Inter({ subsets: ["latin"] });
+
 /** 하위 폴더 경로에서도 별도의 레이아웃 페이지를 생성할 수 있습니다. */
 export default function RootLayout({ children }: RootLayout) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <header className={style.header}>
           <h1>Next 13 Practice Project</h1>
           <nav>
